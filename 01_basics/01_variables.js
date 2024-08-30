f9823a0a9cef57b1700e3a997b0a6374c3695900
safkat07 <safkatsadli@gmail.com>
2024-08-30 16:49:03 +0600

    Var, let, const and data types of js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553;
+let accountEmail = "safkat@gmail.com";
+var accountPassword = "12345";
+accouncCity = "Narayanganj"
+// accountId = 02 // can't chnage const
+console.log(accountId);
+accountEmail = "sc@gmail.com"
+accountPassword = "235234"
+accouncCity = "Dhaka"
+console.table([accountPassword, accountEmail, accouncCity, accountId])
+
---
+++ 02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict" //treat all js as newer version of JavaScript
+alert("Hello") // we are using nodejs, not browser
+
+console.log(3 + 3)
+console.log("Safkat Sharif Sadli")
+
+/*
+<-----Primitive Data Type------>
+number = 2 to the power 53
+bigint
+string = "hello"
+boolean = true/false
+null = standalone value
+undefined = declared but no value is defined
+symbol = unique
+
+<-----Non-Primitive Data Type------>
+object = {
+name: "safkat",
+age: 32}
+array = [1,2,3,]
+*/
