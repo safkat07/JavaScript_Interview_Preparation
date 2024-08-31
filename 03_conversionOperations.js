60cd2f4cc04ab47426abb055db865371ed8ec91d
safkat07 <safkatsadli@gmail.com>
2024-08-31 08:14:44 +0600

    Coversion Operations

---
+++ 03_conversionOperations.js
@@ -0,0 +1,22 @@
+// let score = '33'
+// console.log(typeof score);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+/*
+"33" => 33
+"33ab" => NaN not a number
+true => 1
+*/
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+// let stringNumber = String(someNumber)
+
+console.log(someNumber);
+console.log(typeof someNumber);
+
+
