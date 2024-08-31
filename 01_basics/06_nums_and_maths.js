519c23a6e717cbb1ab46a079c6c1a822d4817009
safkat07 <safkatsadli@gmail.com>
2024-08-31 20:39:20 +0600

    math part explored

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,27 @@
+const score = 400
+const balance = new Number(100)
+// console.log(balance.toString());
+// console.log(balance.toFixed(0));
+
+const otherNumber = 23.3475
+// console.log(otherNumber.toPrecision(2));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-in'));
+
+//maths
+
+// console.log(Math.abs(-40));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor(4.9));
+// const dice = (Math.random() * 10) + 1
+// console.log(dice.toFixed(0));
+
+const min = 1
+const max = 6
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
