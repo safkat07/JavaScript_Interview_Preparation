ce67909cb3d889f39bcb99b221f6c354bb5adc6b
Safkat Sharif <safkatsadli@gmail.com>
2024-09-11 07:23:09 +0600

    For Loop explored with keywords

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,6 +1,53 @@
 //for loop
 
-for(let i = 0; i<10; i++){
-    console.log(i);
-    
+for (let i = 0; i < 10; i++) {
+
+    if (i == 5) {
+        // console.log("5 is the best number"); 
+    }
+    // console.log(i);
+}
+
+//nested loop
+for (let i = 0; i <= 10; i++) {
+    // console.log(`Outer loop value: ${i}`);
+
+    for (let j = 1; j <= 10; j++) {
+        // console.log(`Inner loop value: ${j} and outer loop value: ${i}`);
+        // console.log(`${i} * ${j} = ${i*j}`);
+
+    }
+}
+
+//for loop with array
+
+let myArray = ['flash', 'batman', 'superman']
+
+for (let i = 0; i < myArray.length; i++) {
+    const element = myArray[i];
+    // console.log(element);
+}
+
+
+//keywords in loop: break & continue
+
+
+//break keyword
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        // console.log("5 Deceected");
+        break
+    }
+    // console.log(`the value is ${index}`);
+}
+
+
+//continue in for loop
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log("5 Deceected");
+        continue
+    }
+    console.log(`the value is ${index}`);
 }
