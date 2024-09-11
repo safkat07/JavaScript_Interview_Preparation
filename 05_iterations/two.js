b0f4e38c4c8b51a960eed652a25a471bb94cbd0d
Safkat Sharif <safkatsadli@gmail.com>
2024-09-11 07:41:33 +0600

    While and Do While loops explored

---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+//While Loop
+let index = 2
+
+while (index <= 10) {
+    // console.log(`Value of index ${index}`);
+    index++;
+}
+
+//while Loop with array
+let myArray = ['flash', 'kallu', 'sallu']
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`Favourite ${myArray[arr]}`);
+    arr++;
+}
+
+//do while loop
+
+let score = 1
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <= 10)
