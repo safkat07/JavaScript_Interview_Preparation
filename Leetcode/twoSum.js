4a8505354762807d549bf23c3c6888377feb2a99
Safkat Sharif Sadli <safkatsharif@gmail.com>
2024-09-23 21:14:04 +0600

    two sum leetcode problem answer added

---
+++ Leetcode/twoSum.js
@@ -0,0 +1,28 @@
+
+//burte force
+
+var twoSum = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                let a = [i, j]
+                return a
+            }
+        }
+    }
+    return [];
+};
+
+//one pass hash table
+
+var twoSum = function (nums, target) {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+        map.set(nums[i], i);
+    }
+    return [];
+};
